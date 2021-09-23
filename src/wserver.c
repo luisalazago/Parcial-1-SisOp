8e56a7000704da9a1b49b85edb2e699c3e739245
Luis <luisalazago@gmail.com>
2021-09-23 13:53:39 -0400

    a

--- src/wserver.c
+++ src/wserver.c
@@ -64,7 +64,7 @@ int get_memory(){
 	
 	int *data = (int *)mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
 	//Codigo para ponerlo en sqllite
-	insert_database(data);
+	//insert_database(data);
 	printf("Leyendo de la memoria\n");
 	for (int i = 0; i < NUM; ++i) {
         printf("%d\n", data[i]);
