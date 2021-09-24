267d42d9ded82b3f2d993ce83e85c92586e4fb62
luisalazago <luisalazago@gmail.com>
2021-09-23 22:11:26 -0500

    n requests

---
+++ src/test.py
@@ -0,0 +1,17 @@
+# This is a code to test the concurrence of the server.
+
+import requests
+
+def main():
+    n = int(input("Put an N: "))
+    URL = "http://localhost:9090/"
+    k = 1
+    for i in range(n):
+        response = requests.get(URL)
+        if(response.json()):
+            if(k == 1): print("You made 1 request succesefully.")
+            else: print("You made {} requests succesefully.".format(k))
+            k += 1
+        else: print("Error trying to process a request :/")
+
+main()
